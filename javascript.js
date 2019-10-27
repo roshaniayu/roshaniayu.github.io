d5bac061c1e25f0f5d52bb3fdae0d221ef352345
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2019-10-27 16:54:03 +0700

    finished projects page

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function reveal() {
         document.getElementById("face").src = "images/me.png";
         document.getElementById("reveal").innerHTML = "Hide my face";
     } else {
-        document.getElementById("face").src = "images/zepeto-2.png";
+        document.getElementById("face").src = "images/zepeto-1.png";
         document.getElementById("reveal").innerHTML = "Reveal my face";
     }
 }
